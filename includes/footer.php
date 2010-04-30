3e4e2948682431e53886c60a3072402bfa32b02e
Johnathan <johnathanwilliamson@gmail.com>
2010-04-30 23:23:34 +0100

    Changed FB code

--- includes/footer.php
+++ includes/footer.php
@@ -7,7 +7,7 @@
 				<li><a href="get_images.php">2. Get Images!</a></li>
 			</ul>
 			<h2>Facebook</h2>
-			<iframe src="http://www.facebook.com/plugins/like.php?href=http%3A%2F%2Fwww.facebook.com%2F%3Fsk%3D2361831622%23%21%2Fpages%2FSave-your-pictures-automagically%2F115348351832797%3Fref%3Dts&amp;layout=button_count&amp;show_faces=false&amp;width=200&amp;action=like&amp;colorscheme=light" scrolling="no" frameborder="0" allowTransparency="true" style="border:none; overflow:hidden; width:200px; height:px"></iframe>
+			<iframe src="http://www.facebook.com/plugins/likebox.php?profile_id=115348351832797&amp;width=200&amp;connections=10&amp;stream=false&amp;header=true" scrolling="no" frameborder="0" allowTransparency="true" style="border:none; overflow:hidden; width:200px; height:px"></iframe>
 		</div>
 		<div class="clearer">&nbsp;</div>
 	</div>
